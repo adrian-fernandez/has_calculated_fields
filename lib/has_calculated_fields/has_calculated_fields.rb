2cbd13580dec9c30425a8be442f6a5ecfde65e3c
Adrián Fernández <adrianfernandez85@gmail.com>
2019-08-20 09:40:03 +0200

    Bump to 1.0.2

--- lib/has_calculated_fields/has_calculated_fields.rb
+++ lib/has_calculated_fields/has_calculated_fields.rb
@@ -24,6 +24,7 @@ module HasCalculatedFields
 
     included do
       before_save :_has_calculated_fields__on_before_save
+      before_save :_has_calculated_fields__on_after_save
     end
 
     def _has_calculated_fields__on_before_save
@@ -31,21 +32,45 @@ module HasCalculatedFields
       return unless has_calculated_fields_options.has_key?(:on_before_save)
 
       has_calculated_fields_options[:on_before_save].each do |data|
-        attr_equal = "#{data[:calculated_field]}="
-
-        value = case data[:type]
-                when :date
-                  if data[:format]
-                    I18n.l(_has_calculated_fields__field_value(data), format: data[:format])
-                  else
-                    I18n.l(_has_calculated_fields__field_value(data))
-                  end
-                when :method
-                  data[:method].call(self)
-        end
-
-        send(attr_equal, value)
+        _process_data(data)
       end
+    end
+
+    def _has_calculated_fields__on_after_save
+      return unless respond_to?(:has_calculated_fields_options)
+      return unless has_calculated_fields_options.has_key?(:on_after_save)
+
+      has_calculated_fields_options[:on_after_save].each do |data|
+        _process_data(data)
+      end
+    end
+
+    def _process_data(data)
+      attr_equal = "#{data[:calculated_field]}="
+
+      value = case data[:type]
+              when :date
+                _process_date(data)
+              when :method
+                _process_method(data)
+      end
+
+      send(attr_equal, value)
+    end
+
+    def _process_date(data)
+      value = _has_calculated_fields__field_value(data)
+      return "" if value.blank?
+
+      if data[:format]
+        I18n.l(value, format: data[:format])
+      else
+        I18n.l(value)
+      end
+    end
+
+    def _process_method(data)
+      data[:method].call(self)
     end
 
     def _has_calculated_fields__field_value(data)
